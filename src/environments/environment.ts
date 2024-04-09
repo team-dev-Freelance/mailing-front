83ae65c4955d337c030669c9b5f6c44ffd29f911
Manel Issa <issamanel45@gmail.com>
2024-04-09 16:08:49 +0200

    change apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
   
     //apiUrl: 'http://localhost:8080/api/v1/',
-    apiUrl : 'http://mailing-back-production.up.railway.app/api/v1'
+    apiUrl : 'http://mailing-back-production.up.railway.app/api/v1/'
 };
